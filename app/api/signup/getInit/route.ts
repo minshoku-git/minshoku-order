985ebce22875bb5f4b40ec1fee15ee38fed6d900
sayuriabe101 <s.abe@refact.co.jp>
2025-09-16 19:03:27 +0900

    v0.0.0 一旦退避

--- app/api/signup/getInit/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-import { getSignUpInitData } from '@/app/(public)/signup/_lib/function';
-
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const result = await getSignUpInitData(body);
-  return NextResponse.json(result);
-}
--- app/api/signup/insert/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-import { insertUserProfile } from '@/app/(public)/signup/_lib/function';
-
-export async function PUT(req: NextRequest) {
-  const body = await req.json();
-  const result = await insertUserProfile(body);
-  return NextResponse.json(result);
-}
